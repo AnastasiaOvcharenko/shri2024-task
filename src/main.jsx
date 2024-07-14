6e4210ae3989b16840ad4c35af4705af18287d9f
Анастасия Овчаренко <phiru@inbox.ru>
2024-07-14 23:21:08 +0300

    vite adjustments

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Header, Main, Footer } from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Header />
+    <Main />
+    <Footer />
+  </React.StrictMode>
+);
